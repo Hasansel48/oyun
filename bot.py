61629294e19aca300ac3ff7f26a97e7c4eed1d4c
Hasansel48 <138980612+Hasansel48@users.noreply.github.com>
2025-05-17 22:13:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,8 @@ from telegram.ext import ApplicationBuilder, CommandHandler
 async def start(update, context):
     await update.message.reply_text("Merhaba! Bot çalışıyor.")
 
-app = ApplicationBuilder().token("YOUR_BOT_TOKEN").build()
+app = ApplicationBuilder().token("7781380072:AAHH_4PWe1Nuau3j5fQ49ufLwDNalm66xS8
+").build()
 app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
